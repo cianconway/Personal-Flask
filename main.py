89b77eb6b74e6a0451c93880df0ae646e24c9e73
Cian Conway <cian.conway@outlook.com>
2016-06-05 03:06:54 +0000

    New file added for about page. Header created

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return render_template ("index.html")
 
+@app.route("/about")
+def about():
+    return render_template ("about.html")
+
 if __name__ == "__main__":
     app.run()
