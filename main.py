8cba9819fcd757c34e91c6838cd93d9f9c9ae131
Cian <cian.conway@outlook.com>
2016-06-25 23:42:58 -0400

    Added a resume page which is kind of fucking with the header on the page so that needs to be resolved

--- main.py
+++ main.py
@@ -9,5 +9,9 @@ def hello():
 def about():
     return render_template ("about.html")
 
+@app.route("/resume")
+def resume():
+    return render_template ("resume.html")
+
 if __name__ == "__main__":
     app.run()
