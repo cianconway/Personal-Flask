373e48eb1776c94b79d3ec81cb81294102bde547
Cian <cian.conway@outlook.com>
2016-07-04 01:44:22 -0400

    Added error handling logic to main.py and created a blank error.html

--- main.py
+++ main.py
@@ -17,5 +17,9 @@ def resume():
 def projects():
     return render_template ("projects.html")
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html'), 404
+
 if __name__ == "__main__":
     app.run()
